db1bdbefc2e9f16aac56890fa7c84818ddd19b60
Renish B <renish.bhaskaran@globalenglish.com>
2019-01-09 00:44:49 +0530

    redux store setup

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+//Redux
+import { createStore } from 'redux';
+import rootReducer from './store/reducers/rootReducer';
+
+const store = createStore(rootReducer);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
