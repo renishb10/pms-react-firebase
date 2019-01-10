8fbb17c57e1dfa235b82d1e1bcb65e82caa49ea2
Renish B <renish.bhaskaran@globalenglish.com>
2019-01-10 10:42:15 +0530

    thunk implementation

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 //Redux
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './store/reducers/rootReducer';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
